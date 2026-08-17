a96e65e1eb38c9fe31f1a5324ad42c6768fd0c78
Angel Singh Shrestha <138711417+AngelShresthaCS@users.noreply.github.com>
2026-08-16 19:52:08 -0500

    API Testing Job added to Github Actions Workflow

--- backend/app/tests/test_notes.py
+++ backend/app/tests/test_notes.py
@@ -1,12 +1,19 @@
 import os
-from dotenv import load_dotenv
 
-import pytest 
+from dotenv import load_dotenv
 from fastapi.testclient import TestClient
 
 load_dotenv()
+os.environ.setdefault("MONGODB_URL", "mongodb://localhost:27017")
+
+from app.main import app
+
+client = TestClient(app)
+
+
 def test_root():
     res = client.get("/")
-    get = res.json().get("message")
+    message = res.json().get("message")
+    print("passed")
     assert res.status_code == 200
-    assert get == "FastAPI + AsyncMongoClient is running. Try GET /ping-db"
+    assert message == "FastAPI + AsyncMongoClient is running. Try GET /ping-db"
